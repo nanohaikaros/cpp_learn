00a1797245c6988aee6472bad0d9f9d258b770df
Nanoha <1371689491@qq.com>
2019-06-02 21:13:49 +0800

    3

---
+++ 3/3_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    const double Pi = 22.0 / 7;
+    cout << "The value of constant Pi is: " << Pi << endl;
+
+    // uncomment next line to fail compilation
+    // Pi = 345;
+
+    return 0;
+}
