0ea8c5d851c321b6b8cca8c81b139e487b427ef5
Nanoha <1371689491@qq.com>
2019-06-08 20:49:55 +0800

    7

---
+++ 7/7_6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+const double Pi = 3.14159;
+
+void QueryAndCalculate() {
+    cout << "Enter radius: ";
+    double Radius = 0;
+    cin >> Radius;
+
+    cout << "Area: " << Pi * Radius * Radius << endl;
+
+    cout << "Do you wish to calculate circumference (y/n)? ";
+    char CalcCircum = 'n';
+    cin >> CalcCircum;
+
+    if (CalcCircum == 'n')
+        return;
+
+    cout << "Circumference: " << 2 * Pi * Radius << endl;
+    return;
+}
+
+int main() {
+    QueryAndCalculate();
+
+    return 0;
+}
