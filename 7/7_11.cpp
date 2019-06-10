c3f64225f0d7081ca0b1b2b6d57c21922cae7253
Nanoha <1371689491@qq.com>
2019-06-10 10:14:53 +0800

    7

---
+++ 7/7_11.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void DisplayNums(vector<int>& DyArray) {
+    for_each(DyArray.begin(), DyArray.end(), [](int Element) { cout << Element << " "; });          // lambda
+
+    cout << endl;
+}
+
+int main() {
+    vector<int> MyNumbers;
+    MyNumbers.push_back(501);
+    MyNumbers.push_back(-1);
+    MyNumbers.push_back(25);
+    MyNumbers.push_back(-35);
+
+    DisplayNums(MyNumbers);
+
+    cout << "Sorting them in descending order" << endl;
+
+    sort(MyNumbers.begin(), MyNumbers.end(), [](int Num1, int Num2) { return (Num2 < Num1); });
+
+    DisplayNums(MyNumbers);
+
+    return 0;
+}
