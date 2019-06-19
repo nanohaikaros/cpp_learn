14a52ff1de034a8610fbdf3a37ea303b24046561
nanohaikaros <1371689491@qq.com>
2019-06-19 16:41:39 +0800

    11

---
+++ 11/11_8.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+    public:
+        Animal() {
+            cout << "Animal constructor" << endl;
+        }
+
+        // sample method
+        int Age;
+};
+
+class Mammal : public virtual Animal {
+
+};
+
+class Bird : public virtual Animal {
+
+};
+
+class Reptile : public virtual Animal {
+
+};
+
+class Platypus : public Mammal, public Bird, public Reptile {
+    public:
+        Platypus() {
+            cout << "Platypus constructor" << endl;
+        }
+};
+
+int main() {
+    Platypus duckBilledP;
+    // no compile error as there is only one Animal::Age
+    duckBilledP.Age = 25;
+}
